9bc8cc7a31705429c469a8eee2198e766835a229
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-08-22 12:51:49 +0300

    Update Packet.py

--- rakpy/protocol/Packet.py
+++ rakpy/protocol/Packet.py
@@ -1,1 +1,29 @@
+from rakpy.BitStream import BitStream
 
+class Packet(BitStream):
+    sendTime = None
+    
+    def encodeHeader(self):
+        self.putByte(self.ID)
+        
+    def encodePayload(self): pass
+        
+    def encode(self):
+        self.reset()
+        self.encodeHeader()
+        self.encodePayload()
+        
+    def decodePayload(self): pass
+    
+    def decodeHeader(self):
+        return self.getByte()  # PID
+    
+    def decode(self):
+        self.offset = 0
+        self.decodeHeader()
+        self.decodePayload()
+    
+    def clean(self):
+        self.buffer = None
+        self.offset = 0
+        self.sendTime = None
