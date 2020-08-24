df4fab759b8e1411a59b10e0e6c482c1b9ad3393
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-08-25 01:55:14 +0300

    Update Server.py

--- rakpy/Server.py
+++ rakpy/Server.py
@@ -1,19 +1,30 @@
+from rakpy.BitStream import BitStream
 from rakpy.protocol.ConnectedPing import ConnectedPing
 from rakpy.protocol.ConnectedPong import ConnectedPong
 from rakpy.protocol.UnconnectedPing import UnconnectedPing
 from rakpy.protocol.UnconnectedPingOpenConnection import UnconnectedPingOpenConnection
 from rakpy.protocol.UnconnectedPong import UnconnectedPong
+from rakpy.ServerSocket import ServerSocket
 
 class Server:
-    def rawPacket(self, pid, packet):
-        return bytes([pid]) + packet
-    
     def getPID(self, data):
         return data[0]
+    
+    def sendPacket(self, pk, address):
+        pk.encode()
+        buffer = BitStream.getBuffer()
+        ServerSocket.putPacket(buffer, address)
+        
+    def sendRawPacket(self, pk, address):
+        pk.encode()
+        BitStream.buffer = ord(pk.PID) + BitStream.getBuffer()
+        buffer = BitStream.getBuffer()
+        ServerSocket.putPacket(buffer, address)
     
     def handle(self, data, address):
         pid = getPID(data)
         newPacket = None
         if pid == UnconnectedPing.PID:
             pk = UnconnectedPing()
+            self.sendRawPacket(pk, address)
     
