1c81811c9154cc7f341b7b98d8615c65d11c6bfe
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-08-25 01:13:26 +0300

    Update Server.py

--- rakpy/Server.py
+++ rakpy/Server.py
@@ -1,2 +1,11 @@
 class Server:
-    pass
+    def rawPacket(self, pid, packet):
+        return bytes([pid]) + packet
+    
+    def getPID(self, data):
+        return data[0]
+    
+    def handler(self, data, address):
+        pid = getPID(data)
+        pk = None
+    
