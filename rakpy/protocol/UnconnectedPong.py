043e897493df17878a84a364e548c91e5edfd1ab
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-13 22:17:37 +0300

    Update UnconnectedPong.py

--- rakpy/protocol/UnconnectedPong.py
+++ rakpy/protocol/UnconnectedPong.py
@@ -2,20 +2,20 @@ from rakpy.protocol.Packet import Packet
 from rakpy.protocol.PacketIdentifiers import PacketIdentifiers
 
 class UnconnectedPong(Packet):
-    PID = PacketIdentifiers.UnconnectedPong
+    id = PacketIdentifiers.UnconnectedPong
     
     timeStamp = None
-    serverID = None
+    serverId = None
     serverName = None
     
     def encodePayload(self):
         self.putLong(self.timeStamp)
-        self.putLong(self.serverID)
+        self.putLong(self.serverId)
         self.putMagic()
         self.putString(self.serverName)
         
     def decodePayload(self):
         self.timeStamp = self.getLong()
-        self.serverID = self.getLong()
+        self.serverId = self.getLong()
         self.magic = self.getMagic()
         self.serverName = self.getString()
