28082d68d861444c31df811499e7a06f9dd4d96c
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-13 22:12:44 +0300

    Update Packet.py

--- rakpy/protocol/Packet.py
+++ rakpy/protocol/Packet.py
@@ -1,10 +1,57 @@
-from abc import ABCMeta, abstractmethod
+from binutilspy.BinaryStream import BinaryStream
+from rakpy.InternetAddress import InternetAddress
 
-from rakpy.BitStream import BitStream
+class Packet(BinaryStream):
+    id = -1
+    
+    def readString(self):
+        self.get(self.getShort())
 
-class Packet(BitStream):
-    __metaclass__ = ABCMeta
-    sendTime = None
+    def writeString(self, value):
+        self.putShort(len(value))
+        self.put(value.encode())
+    
+    def getAddress(self):
+        ver = self.getByte()
+        if ver == 4:
+            addr = ".".join([
+                str((~self.getByte()) & 0xff),
+                str((~self.getByte()) & 0xff),
+                str((~self.getByte()) & 0xff),
+                str((~self.getByte()) & 0xff)
+            ])
+            port = self.getShort()
+            InternetAddress(addr, port, ver)
+        elif ver == 6:
+            self.getLShort()
+            port = this.getShort()
+            self.getInt()
+            addr = socket.inet_ntop(socket.AF_INET6, self.get(16))
+            self.getInt()
+            InternetAddress(addr, port, ver)
+        else:
+            raise Exception(f"Unknown address version {ver}")
+
+    def putAddress(self, address: InternetAddress):
+        ver = address.getVersion()
+        addr = address.getAddress()
+        port = address.getPort()
+        self.putByte(ver)
+        if ver == 4:
+            parts = str(addr).split(".")
+            partsLength = len(parts)
+            assert partsLength == 4, f"Expected address length: 4, got {partsLength}"
+            for part in parts:
+                self.putByte((~(int(part))) & 0xff)
+            self.putShort(port)
+        elif ver == 6:
+            self.putLShort(socket.AF_INET6)
+            self.putShort(port)
+            self.putInt(0)
+            self.put(socket.inet_pton(socket.AF_INET6, addr))
+            self.putInt(0)
+        else:
+            raise Exception(f"Unknown address version {ver}")
     
     def encodeHeader(self):
         self.putByte(self.PID)
@@ -27,8 +74,3 @@ class Packet(BitStream):
         self.offset = 0
         self.decodeHeader()
         self.decodePayload()
-    
-    def clean(self):
-        self.buffer = None
-        self.offset = 0
-        self.sendTime = None
