9f8db66dd0faa2b6e5c64914b70efa1437c2909d
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-13 23:17:16 +0300

    Update testserver.py

--- testserver.py
+++ testserver.py
@@ -1,5 +1,6 @@
 from rakpy.Server import Server
+from rakpy.InternetAddress import InternetAddress
 
-server = Server(("0.0.0.0", 19132))
+server = Server(InternetAddress("0.0.0.0", 19132))
 server.setOption("name", "MCPE;My server;407;1.16.0;0;0;0;MyServer;0")
 server.run()
