288f1e9356290de348a71dc06d9fb24b62c3579e
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-14 23:22:14 +0300

    Update PacketIdentifiers.py

--- rakpy/protocol/PacketIdentifiers.py
+++ rakpy/protocol/PacketIdentifiers.py
@@ -1,16 +1,18 @@
 class PacketIdentifiers:
-    UnconnectedPing = 0x01
-    UnconnectedPingOpenConnection = 0x02
-    UnconnectedPong = 0x1c
     ConnectedPing = 0x00
+    UnconnectedPing = 0x01
+    UnconnectedPong = 0x1c
+    UnconnectedPingOpenConnection = 0x02
     ConnectedPong = 0x03
     OpenConnectionRequest1 = 0x05
     OpenConnectionReply1 = 0x06
     OpenConnectionRequest2 = 0x07
     OpenConnectionReply2 = 0x08
+    GamePacket = 0x8e
     ConnectionRequest = 0x09
     ConnectionRequestAccepted = 0x10
+    NewIncomingConnection = 0x13
+    DisconnectNotification = 0x15
     IncompatibleProtocol = 0x19
-    GamePacket = 0x8e
     NacknowledgePacket = 0xa0
     AcknowledgePacket = 0xc0
