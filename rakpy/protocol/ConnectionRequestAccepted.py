2f76dbd68d2d369d038430c27e97ce5bbf511900
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-16 06:57:17 +0300

    Create ConnectionRequestAccepted.py

---
+++ rakpy/protocol/ConnectionRequestAccepted.py
@@ -0,0 +1,25 @@
+from rakpy.protocol.Packet import Packet
+from rakpy.protocol.PacketIdentifiers import PacketIdentifiers
+
+class ConnectionRequestAccepted(Packet):
+    id = PacketIdentifiers.ConnectionRequestAccepted
+    
+    clientAddress = None
+    systemIndex = None
+    internalId = None
+    requestTime = None
+    time = None
+    
+    def encodePayload(self):
+        self.putLong(self.clientAddress)
+        self.putShort(self.systemIndex)
+        
+        self.putLong(self.requestTime)
+        self.putLong(self.time)
+        
+    def decodePayload(self):
+        self.clientAddress = self.getLong()
+        self.systemIndex = self.getShort()
+        
+        self.requestTime = self.getLong()
+        self.time = self.getLong()
