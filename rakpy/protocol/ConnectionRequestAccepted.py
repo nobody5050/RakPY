034d046319789f3f227c432b5574a5c3f26fccd6
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-16 15:04:34 +0300

    Update ConnectionRequestAccepted.py

--- rakpy/protocol/ConnectionRequestAccepted.py
+++ rakpy/protocol/ConnectionRequestAccepted.py
@@ -6,20 +6,22 @@ class ConnectionRequestAccepted(Packet):
     
     clientAddress = None
     systemIndex = None
-    internalId = None
+    internalId = []
     requestTime = None
     time = None
     
     def encodePayload(self):
         self.putAddress(self.clientAddress)
         self.putShort(self.systemIndex)
-        
+        for i in range(0, 20):
+            self.putAddress(self.internalId[i])
         self.putLong(self.requestTime)
         self.putLong(self.time)
         
     def decodePayload(self):
         self.clientAddress = self.getAddress()
         self.systemIndex = self.getShort()
-        
+        for i in range(0, 20):
+            self.internalId.append(self.getAddress())
         self.requestTime = self.getLong()
         self.time = self.getLong()
