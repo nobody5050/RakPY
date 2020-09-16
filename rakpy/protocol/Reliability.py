85f86f6486c06e87714dd81182b7c6c65d9a90b9
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-16 16:22:35 +0300

    Create Reliability.py

---
+++ rakpy/protocol/Reliability.py
@@ -0,0 +1,9 @@
+class Reliability:
+    unreliable = 0
+    unreliableSequenced = 1
+    reliable = 2
+    reliableOrdered = 3
+    reliableSequenced = 4
+    unreliableAckReceipt = 5
+    reliableAckReceipt = 6
+    reliableOrderedAckReceipt = 7
