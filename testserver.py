90295d4b996a9a7b2949576109d3938e62c8596b
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-01 14:37:53 +0300

    improved test server

--- testserver.py
+++ testserver.py
@@ -1,19 +1,37 @@
 from rakpy.server.Server import Server
+from rakpy.server.ServerInterface import ServerInterface
 from rakpy.utils.InternetAddress import InternetAddress
 from rakpy.utils.MinecraftServerName import MinecraftServerName
 
-server = Server(InternetAddress("0.0.0.0", 19132))
-
-mcsn = MinecraftServerName()
-mcsn.edition = "MCPE"
-mcsn.name = "MyServer"
-mcsn.motd = "MyServer"
-mcsn.protocol = 408
-mcsn.version = "1.16.20"
-mcsn.players["online"] = 0
-mcsn.players["max"] = 0
-mcsn.gamemode = "Creative"
-mcsn.serverId = 0
-name = mcsn.toString()
-
-server.name = name
+class TestServer(ServerInterface):
+    server = None
+    name = None
+    
+    def __init__(self):
+        self.server = Server(InternetAddress("0.0.0.0", 19132), self)
+        self.name = self.makeName()
+        self.server.name = self.name
+        
+    def makeName(self):
+        mcsn = MinecraftServerName()
+        mcsn.edition = "MCPE"
+        mcsn.name = "MyServer"
+        mcsn.motd = "MyServer"
+        mcsn.protocol = 408
+        mcsn.version = "1.16.20"
+        mcsn.players["online"] = 0
+        mcsn.players["max"] = 0
+        mcsn.gamemode = "Creative"
+        mcsn.serverId = 0
+        return mcsn.toString()
+      
+    def onOpenConnection(self, connection):
+        print("OPEN_CONNECTION")
+        
+    def onCloseConnection(self, address, reason):
+        print("CLOSED_CONNECTION")
+        
+    def onEncapsulated(self, packet, address):
+        print("ENCAPSULATED")
+        
+TestServer()
